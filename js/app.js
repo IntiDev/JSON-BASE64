28cce3883777caf53897be33486edb5a7b421458
IntiDev <yeseniase@outlook.it>
2017-12-19 14:23:23 -0600

    Se genera archivo y link para el .json

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@
         document.getElementById("img-content").src= e.target.result;
         // document.getElementById("b64").innerHTML = e.target.result;
         arrayBase64.push(e.target.result);
+        // console.log(JSON.stringify(e.target.result));
         // arrayBase64["yyy"] = e.target.result.split(",");
       });
       imageLoad.readAsDataURL( this.files[0] );
@@ -32,7 +33,22 @@
       // "content": xxx[1]
     }
     console.log(objectFile);
+    // CODIGO PARA DESCARGAR ARCHIVO
+    const imgName = objectFile.name;
+    const fileName = imgName.split(".");
+    // console.log(fileName
+    const linkDownload = document.getElementById("link");
+    linkDownload.setAttribute("download", fileName[0] + ".json");
+    // linkDownload.addEventListener("click", downloadJSON);
+    linkDownload.addEventListener("click", function(code) {
+      this.href = 'data:text/javascript;charset=utf-8,'
+      + encodeURIComponent(objectFile);
+    });
   }
 
+  // const downloadJSON = function(code) {
+  //   this.href = 'data:text/plain;charset=utf-8,'
+  //   + encodeURIComponent(objectFile);
+  // };
   document.getElementById("input-file").addEventListener("change", readFile);
 })()
